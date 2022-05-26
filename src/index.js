a703828e88e441f7ce0fbfe9906026229267f420
BARI-Zakaria <zakariabari213@gmail.com>
2022-05-26 14:56:03 +0100

    TodoList App completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import TodoInput from './Components/TodoInput';
+import TodoList from './Components/TodoList';
 import reportWebVitals from './reportWebVitals';
+import "./Components/TodoList.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <TodoList />
   </React.StrictMode>
 );
 
